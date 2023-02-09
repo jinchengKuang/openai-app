51f05bca89c84720609c0dd49ed58fff060c5d63
Jincheng Kuang <jkuang37@gatech.edu>
2023-02-09 15:32:13 -0500

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async e => {
 	// messageDiv.innerHTML = "..."
 	loader(messageDiv);
 
-	const response = await fetch('http://localhost:5500', {
+	const response = await fetch('https://openai-app-8w9y.onrender.com', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
